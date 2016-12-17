05c1166e913b1c37e26e250b3e2ce7f8ac9b0987
Joseph Dickens <josephdickens87@gmail.com>
2016-12-17 15:50:17 -0500

    2nd activity now added

--- app/src/main/java/com/example/joe/leasetracker/MainActivity.java
+++ app/src/main/java/com/example/joe/leasetracker/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.joe.leasetracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    TextView instructionText;
+    EditText leaseDate;
+    EditText milesYear;
+    EditText leaseMonths;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,6 +39,22 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
+        leaseDate = (EditText) findViewById(R.id.leaseDate);
+        milesYear = (EditText) findViewById(R.id.milesYear);
+        leaseMonths = (EditText) findViewById(R.id.leaseMonths);
+
+        Button calculateButton = (Button) findViewById(R.id.calculateButton);
+        calculateButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
