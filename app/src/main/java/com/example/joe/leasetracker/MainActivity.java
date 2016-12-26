3d920264ec45543b96a8efa98179d1d385d78e47
Joseph Dickens <josephdickens87@gmail.com>
2016-12-25 21:22:21 -0500

    moved a lot of logic outside of click listener.

--- app/src/main/java/com/example/joe/leasetracker/MainActivity.java
+++ app/src/main/java/com/example/joe/leasetracker/MainActivity.java
@@ -1,5 +1,10 @@
+//Experimental
+
 package com.example.joe.leasetracker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -8,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
@@ -17,10 +23,33 @@ import org.joda.time.format.DateTimeFormat;
 
 import java.text.DecimalFormat;
 
-
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    //TextView instructionText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        JodaTimeAndroid.init(this);
+        savebutton=(Button)findViewById(R.id.saveButton);
+        savebutton.setOnClickListener(save);
+        loadButton=(Button)findViewById(R.id.loadButton);
+        loadButton.setOnClickListener(load);
+        calculateButton = (Button)findViewById(R.id.calculateButton);
+        calculateButton.setOnClickListener(calc);
+        leaseDate = (EditText) findViewById(R.id.leaseDate);
+        leaseMonths = (EditText) findViewById(R.id.leaseMonths);
+        milesAllowed = (EditText) findViewById(R.id.milesAllowed);
+        daysLeased = (TextView) findViewById(R.id.daysLeased);
+        expectedMiles = (TextView) findViewById(R.id.expectedMiles);
+        overMiles = (TextView) findViewById(R.id.overMiles);
+        currentMiles = (EditText) findViewById(R.id.currentMiles);
+
+    }
+
+    Button calculateButton;
+    Button savebutton;
+    Button loadButton;
     EditText leaseDate;
     EditText currentMiles;
     EditText leaseMonths;
@@ -30,88 +59,148 @@ public class MainActivity extends AppCompatActivity
     TextView overMiles;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+
+
+    DecimalFormat df2 = new DecimalFormat(".##");
+
+
+    View.OnClickListener save = new View.OnClickListener()
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        JodaTimeAndroid.init(this);
-
-        currentMiles = (EditText) findViewById(R.id.currentMiles);
-        leaseMonths = (EditText) findViewById(R.id.leaseMonths);
-        daysLeased = (TextView) findViewById(R.id.daysLeased);
-        leaseDate = (EditText) findViewById(R.id.leaseDate);
-        expectedMiles = (TextView) findViewById(R.id.expectedMiles);
-        milesAllowed = (EditText) findViewById(R.id.milesAllowed);
-        overMiles = (TextView) findViewById(R.id.overMiles);
-
-        final DecimalFormat df2 = new DecimalFormat(".##");
-        final LocalDateTime today = LocalDateTime.now();
-
-
-        Button calculateButton = (Button) findViewById(R.id.calculateButton);
-        calculateButton.setOnClickListener(new View.OnClickListener()
+        @Override
+        public void onClick(View view)
         {
-            @Override
-            public void onClick(View view)
-            {
-                LocalDateTime jodalease = LocalDateTime.parse(GetLeaseDate(), DateTimeFormat
-                        .forPattern("MM/dd/yyyy"));
-                int currentDaysLeased = Days.daysBetween(jodalease, today).getDays();
 
 
-                int leaseMonthsInt = Integer.parseInt(GetLeaseMonths());
-                LocalDateTime leaseEnd = jodalease.plusMonths(leaseMonthsInt);
-                int leaseLengthDays = Days.daysBetween(jodalease, leaseEnd).getDays();
+            SharedPreferences leaseInfo = getSharedPreferences("LeaseData", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = leaseInfo.edit();
+            editor.putString("leaseDate",leaseDate.getText().toString());
+            editor.putString("leaseMonths",leaseMonths.getText().toString());
+            editor.putString("milesAllowed",milesAllowed.getText().toString());
+
+            editor.apply();
+
+            Toast.makeText(MainActivity.this, "Data Saved", Toast.LENGTH_SHORT).show();
 
 
-                double avgMiles = GetAverageMiles(GetCurrentMiles(), currentDaysLeased);
-                double anticipatedMiles = GetAnticipatedMiles(avgMiles, leaseLengthDays);
+            String leaseString = leaseDate.getText().toString();
+            LocalDateTime today = LocalDateTime.now();
+            LocalDateTime jodalease = LocalDateTime.parse(leaseString, DateTimeFormat
+                    .forPattern("MM/dd/yyyy"));
+            int currentDaysLeased = Days.daysBetween(jodalease, today).getDays();
+
+            //daysLeased.setText("" + currentDaysLeased);
 
 
-                int milesyearInt = Integer.parseInt(GetMilesAllowed());
-                int monthlyAllowedMiles = milesyearInt / 12;
-                int totalAllowedMiles = monthlyAllowedMiles * leaseMonthsInt;
-                double milesOver = anticipatedMiles - totalAllowedMiles;
+            String leaseMonthsInput = leaseMonths.getText().toString();
+            int leaseMonthsInt = Integer.parseInt(leaseMonthsInput);
 
-                daysLeased.setText("" + currentDaysLeased);
-                expectedMiles.setText("" + df2.format(anticipatedMiles));
-                overMiles.setText("" + df2.format(milesOver));
+            LocalDateTime leaseEnd = jodalease.plusMonths(leaseMonthsInt);
+            int leaseLengthDays = Days.daysBetween(jodalease, leaseEnd).getDays();
 
-            }
-        });
 
+            String milesInput = currentMiles.getText().toString();
+            int currMilesInt = Integer.parseInt(milesInput);
+
+            double avgMiles = (currMilesInt / currentDaysLeased);
+            double anticipatedMiles = avgMiles*leaseLengthDays;
+
+
+            //expectedMiles.setText("" + df2.format(anticipatedMiles));
+
+
+            String milesString = milesAllowed.getText().toString();
+            int milesyearInt = Integer.parseInt(milesString);
+            int monthlyAllowedMiles = milesyearInt/12;
+            int totalAllowedMiles = monthlyAllowedMiles*leaseMonthsInt;
+            double milesOver = anticipatedMiles - totalAllowedMiles;
+
+            daysLeased.setText("" + currentDaysLeased);
+            expectedMiles.setText("" + df2.format(anticipatedMiles));
+            overMiles.setText("" + df2.format(milesOver));
+
+        }
+    };
+
+    /*
+    private double GetAverageMiles(String currMiles, int currDays ){
+        return Double.parseDouble(currMiles)/(double)currDays;
     }
+    private double GetAnticipatedMiles(double avgMiles, int leaseLengthDays ){
+        return avgMiles*(double)leaseLengthDays;
+    }
+    */
 
-    private double GetAverageMiles(String currMiles, int currDays)
+
+
+
+
+
+    View.OnClickListener load =new View.OnClickListener()
     {
-        return Double.parseDouble(currMiles) / (double) currDays;
+        @Override
+        public void onClick(View view)
+        {
+            SharedPreferences sharedPreferences = getSharedPreferences("LeaseData", Context
+                    .MODE_PRIVATE);
+            String leaseDate = sharedPreferences.getString("leaseDate", "f");
+            String leaseMonths = sharedPreferences.getString("leaseMonths", "f");
+            String milesAllowed = sharedPreferences.getString("milesAllowed", "f");
+
+            LocalDateTime today = LocalDateTime.now();
+
+
+            String currMilesString = currentMiles.getText().toString();
+            int currMilesInt = Integer.parseInt(currMilesString);
+
+            LocalDateTime jodalease = LocalDateTime.parse(leaseDate, DateTimeFormat
+                    .forPattern("MM/dd/yyyy"));
+            int currentDaysLeased = Days.daysBetween(jodalease, today).getDays();
+            int leaseMonthsInt = Integer.parseInt(leaseMonths);
+            int milesAllowedYearInt = Integer.parseInt(milesAllowed);
+
+            LocalDateTime leaseEnd = jodalease.plusMonths(leaseMonthsInt);
+            int leaseLengthDays = Days.daysBetween(jodalease, leaseEnd).getDays();
+
+
+
+            double avgMiles = (currMilesInt / currentDaysLeased);
+
+            double anticipatedMiles = avgMiles * leaseLengthDays;
+
+
+
+            int monthlyAllowedMiles = milesAllowedYearInt / 12;
+            int totalAllowedMiles = monthlyAllowedMiles * leaseMonthsInt;
+            double milesOver = anticipatedMiles - totalAllowedMiles;
+
+
+
+
+
+            daysLeased.setText("" + currentDaysLeased);
+            expectedMiles.setText("" + df2.format(anticipatedMiles));
+            overMiles.setText("" + df2.format(milesOver));
+        }
+    };
+
+    View.OnClickListener calc = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+
+        }
+    };
+
+    public void next()
+    {
+        Intent next = new Intent(this, Main2Activity.class);
+        startActivity(next);
     }
 
-    private double GetAnticipatedMiles(double avgMiles, int leaseLengthDays)
-    {
-        return avgMiles * (double) leaseLengthDays;
-    }
 
-    private String GetLeaseDate()
-    {
-        return leaseDate.getText().toString();
-    }
 
-    private String GetLeaseMonths()
-    {
-        return leaseMonths.getText().toString();
-    }
 
-    private String GetCurrentMiles()
-    {
-        return currentMiles.getText().toString();
-    }
-
-    private String GetMilesAllowed()
-    {
-        return milesAllowed.getText().toString();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
@@ -138,4 +227,9 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View view)
+    {
+
+    }
 }
